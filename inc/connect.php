e4429ccd23499ce33d3770fe75b9151b38d988aa
kausar-zaman <41296913+kausar-zaman@users.noreply.github.com>
2019-09-04 15:46:32 +0100

    Update connect.php

--- inc/connect.php
+++ inc/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost","unn_w17022892","nufc2010","unn_w17022892");
+$conn=mysqli_connect("localhost","unn_w17022892","********","unn_w17022892");
  if(!$conn)
  {
   die("error in establishing the connection :". mysqli_connect_error());
